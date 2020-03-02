4520a3a4911f5965191bb0c2268a93ff2f567b8b
Dmitry <39702051+MeowEz@users.noreply.github.com>
2020-03-02 19:28:22 +0700

    .
    test

---
+++ mdd.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#mydir=$(pwd)
+#FILE=$mydir/$filename
+value=1
+echo "This program shows information about file"
+while [ $value -gt 0 ]
+do
+        echo "Enter file name:"
+        read filename
+        FILE=$filename
+if test -f "$FILE";
+then
+        stat $FILE
+        echo -e "Would you like to continue? y/n"
+read ans
+if [ $ans != "y" ]
+then exit
+        fi
+else
+        echo "Error. File not found"
+        echo "Error. File not found" 1>&2
+echo -e "Would you like to continue? y/n"
+read ans
+if [ $ans != "y" ]
+then exit
+fi
+fi
+done
